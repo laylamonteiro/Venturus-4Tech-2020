d0fcba4746aff8310b850bc190fe3ce0e414ebc9
laylindo <layla-sep@hotmai.com>
2020-01-27 13:43:07 -0300

    a

--- 4tech-frontend/src/containers/Login/Login.js
+++ 4tech-frontend/src/containers/Login/Login.js
@@ -1,17 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'
+import { Button, TextField } from '@material-ui/core'
 
 //Função
 const Login = ({ name }) => {
     const [login, setLogin] = useState('')
-
-    useEffect(() => {
-        console.log('alterei o login')
-
-    return (() => {
-        console.log('This shit is done')
-    })
-
-    }, [login])
+    const [fullName, setFullName] = useState('')
+    const [userLogin, setUserLogin] = useState('')
 
     const onLoginChange = (event) => {
         console.log(event.target.value)
@@ -20,7 +14,24 @@ const Login = ({ name }) => {
 
     return (
     
-        <div>
+        <div className="login">
+            <form>
+                <TextField 
+                    id="fullName"
+                    label="Full Name"
+                    value={fullName}
+                    onChange={ (event) => setFullName(event.target.value)}
+                    required
+                />
+                <TextField 
+                    id="userLogin"
+                    label="User Login"
+                    value={fullName}
+                    onChange={ (event) => setFullName(event.target.value)}
+                    required
+                />
+                <Button color='primary'>Sign in</Button>
+            </form>
             <input type='text' value={login} onChange={onLoginChange}/>
             Login works {name}
         </div>
